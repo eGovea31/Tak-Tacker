dc9bdf003463738c8f57fef71857e8b4f07764f4
eGovea31 <ernestogovea31@gmail.com>
2026-03-29 15:16:40 -0500

    Save tasks

--- index.js
+++ index.js
@@ -1,7 +1,52 @@
-const args = process.argv
+/*
+INPUT
+  ↓
+interpretar comando
+  ↓
+leer datos existentes
+  ↓
+modificar datos
+  ↓
+guardar datos
+  ↓
+OUTPUT
+*/
+const fs= require("fs")
 
-const command = args[2]
-const input = args[3]
+// ------------Read Tasks-------------
+function loadTasks(){
+    try{
+        const data= fs.readFileSync("tasks.json","utf-8")
+        return JSON.parse(data)
+    } catch(error){
+        return[]
+    }
+}
 
-console.log("Command : " ,command)
-console.log("Text : ", input)
+//------------Save tasks------------------
+function saveTasks(tasks){
+    fs.writeFileSync("tasks.json",JSON.stringify(tasks,null,2))
+}
+
+const args = process.argv // save arguments
+
+const command = args[2] // extract command 
+const input = args[3] // 
+
+if(command === "add"){
+    const tasks = loadTasks()
+
+    const newTask = {
+        id: tasks.length+1,
+        description: input
+    }
+
+    tasks.push(newTask)
+    saveTasks(tasks)
+
+    console.log("Tarea agregada: ", newTask)
+} else {
+    console.log("Unrecognized Command ")
+}
+
+//const tasks=[]
